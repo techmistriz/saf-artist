43b2caa0b49d335b5fbabd608219ae11d762e96c
Ram Yadav <rmy2134430@gmail.com>
2024-04-26 11:41:07 +0530

    axsdfghh

--- resources/views/admin/includes/sidebar/menu.blade.php
+++ resources/views/admin/includes/sidebar/menu.blade.php
@@ -25,9 +25,9 @@
                 @include('admin/includes/sidebar/user')
             @endif        	
             
-        	@if(\Helper::checkPermisson('ArtistMemberController', $rolePermissionArr))
+        	<!-- @if(\Helper::checkPermisson('ArtistMemberController', $rolePermissionArr))
             	@include('admin/includes/sidebar/artist_member')
-            @endif
+            @endif -->
 
             <!-- if(\Helper::checkPermisson('TicketBookingController', $rolePermissionArr))
                 include('admin/includes/sidebar/ticket_booking')
