292fba0135850eb5cd907fbd10a4fcdbc90e1b9a
MY LENOVO <ts.techmistriz@gmail.com>
2024-06-18 11:56:25 +0530

    Navbar design fixed

--- resources/views/frontend/dashboard.blade.php
+++ resources/views/frontend/dashboard.blade.php
@@ -2,28 +2,37 @@
 
 @section('content')
 
-<div class="container container-fluid pb-6">    
-    <div class="card border-0">
-        <nav class="main-navbar">
-        <a href="{{URL::to('/')}}"><img src="{{url('/image/Logo-SVG.svg')}}" alt="Image"/ class="head-logo"></a>
-                <ul class="navbar-menu">
+
+
+
+<div class="container">
+    <div class="navbar-header">
+        <div class="row">
+            <div class="col-md-3">
+                <a href="{{URL::to('/')}}">
+                    <img src="{{url('/image/Logo-SVG.svg')}}" alt="Image"/ class="head-logo">
+                </a>
+            </div>
+            <div class="col-md-9">
+                <ul class="navbar-menu" id="mobileMenu">
                     <li><a href="{{route('home')}}">Home</a></li>
                     <li><a href="{{route('faq.details')}}">FAQs</a></li>
                 </ul>
-            <button class="navbar-toggler" onclick="toggleMobileMenu()">
-                <span class="navbar-toggler-icon" id="mobileIcon">
-                    <span class="first"></span>
-                    <span class="second"></span>
-                    <span class="third"></span></span>
-            </button>
-        </nav>
-    </div>
 
-    <div class="navbar-mobile-menu" id="mobileMenu">
-        <a href="{{route('home')}}">Home</a>
-        <a href="{{route('faq.details')}}">FAQs</a>
+                <button class="navbar-toggler" onclick="toggleMobileMenu()">
+                    <span class="navbar-toggler-icon" id="mobileIcon">
+                        <span class="first"></span>
+                        <span class="second"></span>
+                        <span class="third"></span></span>
+                </button>
+            </div>
+            
+        </div>
     </div>
 </div>
+
+
+
 <script>
   function toggleMobileMenu() {
     var mobileMenu = document.getElementById("mobileMenu");
