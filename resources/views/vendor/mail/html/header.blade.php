642913312849a92af913a04731ff72bb425f3bd1
Ram Yadav <rmy2134430@gmail.com>
2024-07-11 11:53:23 +0530

    logo change

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 	<td class="header">
 		<a href="{{ route('login') }}" style="display: inline-block;">
 
-			<img src="http://artist24.demoserver.co.in/media/logos/logo-white.png" class="logo" alt="{{ env('APP_NAME', 'Laravel') }}" style="height: 60px; width:auto;">
+			<img src="http://artist24.demoserver.co.in/media/logos/Logo-SVG.svg" class="logo" alt="{{ env('APP_NAME', 'Laravel') }}" style="height: 60px; width:auto;">
 			
 		</a>
 	</td>
