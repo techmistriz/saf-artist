3a067eab64ffed0534e0bb71622f36199811f884
Ram Yadav <rmy2134430@gmail.com>
2024-07-31 22:10:31 +0530

    2024

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -3,7 +3,7 @@
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <head>
 	<meta charset="utf-8" />
-	<title>SAF 2023</title>
+	<title>SAF 2024</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
 	<!--end::Fonts-->
